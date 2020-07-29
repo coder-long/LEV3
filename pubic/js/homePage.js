2795f112939034573260551dcf0917e21a174ff0
tangtang <1849601712@qq.com>
2020-07-29 10:11:40 +0800

    首页静态页面完成

---
+++ pubic/js/homePage.js
@@ -0,0 +1,16 @@
+/* ---------登录框显示------------ */
+$('.uc-my').on('click', function() {
+    $('.pop-box').show();
+});
+/* ---------登录框隐藏------------ */
+$('.pop-close').on('click', function() {
+    $('.pop-box').hide();
+});
+/* ----------注册框显示-------------------- */
+$('.uc-zhuce').on('click', function() {
+    $('.zhuce-box').show();
+});
+/* ----------注册框隐藏--------------------- */
+$('.pop-close').on('click', function() {
+    $('.zhuce-box').hide();
+});
