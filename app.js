10a6cd4dc9beae37f14b58c0fc5ce3f8322e7eb1
何龙 <2935842944@qq.com>
2020-07-29 10:18:35 +0800

    跨域

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.post('/api/login',(req,res)=>{
 //搜索
 app.get('/api/search',(req,res)=>{
 
-  let char_type = req.query.char_type  //传的参数
+  let char_type =req.query.char_type //传的参数
 
   Vehicle.find({char_type:char_type},(err,vehicle)=>{
 
@@ -117,11 +117,7 @@ app.get('/api/search',(req,res)=>{
 
 // Vehicle.find({}).then(res => console.log(res))
 
- Vehicle.find({char_type:'本田 思域 2019款 220TURBO CVT劲动版 国VI'},(err,vehicle)=>{
-
-  console.log(vehicle);
-
-})
+//  Vehicle.find({char_type:/奥迪/}).then(res=>console.log(res))
 
 // Vehicle.remove({char_type:'本田 思域 2019款 220TURBO CVT劲动版 国VI'}).then(res => console.log(res))  
 
@@ -130,7 +126,12 @@ app.get('/api/search',(req,res)=>{
 //分页
 app.post('/api/page',(req,res)=>{
 
-  Vehicle.find({}).then((data)=>{
+  let page = req.body.page
+
+  Vehicle.find({})
+  .skip(40*page)
+  .limit(40)
+  .then((data)=>{
     res.send({
       code:0,
       msg:'成功',
@@ -186,9 +187,9 @@ app.post('/api/hot',(req,res)=>{
   })
 
 })
-Vehicle.find({now_price:{ $lt: '15万'}}).limit(12).then((data)=>{
-  console.log(data);
-})
+// Vehicle.find({now_price:{ $lt: '15万'}}).limit(12).then((data)=>{
+//   console.log(data);
+// })
 
 
 
